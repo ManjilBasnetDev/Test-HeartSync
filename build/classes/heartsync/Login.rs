c1a2f2479ad32b6d83e8a6e271ad3d99f8157a23
ManjilBasnetDev <dev.manjil.c@gmail.com>
2025-06-11 18:48:14 +0545

    Removed compiled and IDE files; updated .gitignore

--- build/classes/heartsync/Login.rs
+++
@@ -1,5 +0,0 @@
-heartsyncdatingapp.model.LoginFinal$2
-heartsyncdatingapp.model.LoginFinal$1
-heartsyncdatingapp.model.LoginFinal$4
-heartsyncdatingapp.model.LoginFinal$3
-heartsyncdatingapp.model.LoginFinal
--- build/classes/heartsync/LoginFinal.rs
+++
@@ -1,5 +0,0 @@
-heartsync.view.LoginFinal$1
-heartsync.view.LoginFinal
-heartsync.view.LoginFinal$2
-heartsync.view.LoginFinal$3
-heartsync.view.LoginFinal$4
